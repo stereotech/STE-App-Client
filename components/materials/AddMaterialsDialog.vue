7cb75e5a1f0a7587c0dbf200e33542d3b3c691ad
Ivashkina <ivashkina97@rambler.ru>
2019-03-18 16:16:07 +0400

    Added form frame for adding material.

---
+++ components/materials/AddMaterialsDialog.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <v-btn color="primary" large round block @click="showForm">
+      <v-icon>mdi-plus</v-icon>&nbsp;Add material
+    </v-btn>
+    <v-dialog
+      v-model="dialog"
+      max-width="425"
+      persistent
+      :fullscreen="$vuetify.breakpoint.xs"
+      scrollable
+    >
+      <v-card>
+        <v-toolbar dark color="primary">
+          <v-btn icon dark @click="closeDialog">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Add material</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text style="height: 1000px;">
+          <v-container grid-list-md fluid>
+            <v-flex d-flex xs12>
+              <div class="title text-xs-center">
+                <v-progress-circular :size="50" color="primary" indeterminate></v-progress-circular>
+              </div>
+            </v-flex>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import { Getter, Action, Mutation, namespace } from 'vuex-class'
+
+const scannerResult = namespace('addMaterialState')
+
+@Component
+export default class AddMaterialsDialog extends Vue {
+  private dialog: boolean = false
+  private confirmation: boolean = false
+
+  async showForm () {
+    this.dialog = true
+  }
+
+  private closeDialog () {
+    this.dialog = false
+  }
+}
+</script>
+
+<style>
+</style>
--- pages/materials/index.vue
+++ pages/materials/index.vue
@@ -1,36 +1,54 @@
 <template>
-  <v-flex xs12>
-    <firstLaunchStepper/>
-  </v-flex>
+  <v-layout row wrap>
+    <v-flex xs12>
+      <PrinterCard
+              toolbar
+              controlPanel
+              v-for="printer in printers"
+              :id="printer.id"
+              :key="printer.id"
+      />
+      <AddMaterialsDialog/>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator'
-import firstLaunchStepper from 'components/wizards/firstLaunch/Stepper.vue'
+  import { Vue, Component } from 'nuxt-property-decorator'
+  import PrinterCard from '~/components/common/PrinterCard.vue'
+  import AddMaterialsDialog from '~/components/materials/AddMaterialsDialog.vue'
+  import { Action, Getter, namespace } from 'vuex-class'
+  import { PrinterInfo, PrinterStatus } from 'types/printer'
 
-// @Component({
-//   layout: 'solid',
-//   components: {
-//     firstLaunchStepper
-//   }
-// })
-// export default class extends Vue {
-//   private pollingStatus!: NodeJS.Timeout
-//
-//   private async pollData () {
-//     this.pollingStatus = setInterval(async () => {
-//       await this.$store.dispatch('printersState/fetchStatus')
-//     }, 1000)
-//   }
-//
-//   async mounted () {
-//     await this.pollData()
-//   }
-//
-//   beforeDestroy () {
-//     clearInterval(this.pollingStatus)
-//   }
-// }
+  const printers = namespace('printersState')
+
+  @Component({
+    components: {
+      PrinterCard,
+      AddMaterialsDialog
+    }
+  })
+  export default class PrintersPage extends Vue {
+    @printers.Getter printers!: PrinterInfo[]
+    @printers.Getter printersStatus!: PrinterStatus[]
+
+    private pollingStatus!: NodeJS.Timeout
+
+    private async pollData () {
+      await this.$store.dispatch('printersState/fetchPrinters')
+      this.pollingStatus = setInterval(async () => {
+        await this.$store.dispatch('printersState/fetchStatus')
+      }, 1000)
+    }
+
+    async mounted () {
+      await this.pollData()
+    }
+
+    beforeDestroy () {
+      clearInterval(this.pollingStatus)
+    }
+  }
 </script>
 
 <style>
