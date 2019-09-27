01bb1498aa786af4b636d56cce0914a088d9c53a
frylock34 <frylock3434@gmail.com>
2019-09-27 09:29:12 +0400

    remove replace address

--- replace-printer-address.js
+++
@@ -1,9 +0,0 @@
-var replace = require('replace'),
-    path = require("path");
-
-replace({
-    regex: 'http:\/\/192.[0-9].........api\/',
-    replacement: '/api/',
-    paths: ['store/index.ts'],
-    recursive: false
-});
