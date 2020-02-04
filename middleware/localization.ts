fdd0367722ca16a1201ffb342753fd0b80cfb7bf
Ilya Gushchin <frylock3434@gmail.com>
2020-02-04 13:17:56 +0400

    edit localization

--- middleware/localization.ts
+++ middleware/localization.ts
@@ -2,7 +2,7 @@ import { Middleware } from '@nuxt/types'
 
 const localization: Middleware = async (context) => {
     let settings = context.store.state.settingsState.settings
-    if (settings && context.app.i18n) {
+    if (settings && context.app.i18n && context.app.i18n.locale !== settings.locale) {
         context.app.i18n.setLocale(settings.language)
         context.app.$moment.locale(settings.language)
     }
