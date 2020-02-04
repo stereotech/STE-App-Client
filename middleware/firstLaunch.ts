dcfa2c408be36ac303d9ff76b8d8c930457dbef1
Ilya Gushchin <frylock3434@gmail.com>
2020-02-04 15:20:25 +0400

    turn language autodetec off

--- middleware/firstLaunch.ts
+++ middleware/firstLaunch.ts
@@ -11,7 +11,7 @@ const firstLaunch: Middleware = async (context) => {
   await context.store.dispatch('settingsState/fetchSettings')
   let settings = context.store.state.settingsState.settings
   if (settings && context.app.i18n && context.app.i18n.locale !== settings.language) {
-    context.app.i18n.setLocale(settings.language)
+    await context.app.i18n.setLocale(settings.language)
     context.app.$moment.locale(settings.language)
   }
   if (settings.firstLaunch) {
