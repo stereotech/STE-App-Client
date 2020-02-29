7ef43aa7da0f63827a9ea1d24369806bfa1683f8
Kate <katya.romakh@yandex.ru>
2020-02-29 23:10:48 +0400

    работа с папками

--- components/dashboard/Storage.vue
+++ components/dashboard/Storage.vue
@@ -7,16 +7,62 @@
           <span v-if="name" class="headline font-weight-light">&nbsp;{{$t('at ')}} {{ display }}</span>
         </v-card-title>
         <v-spacer />
+        <v-spacer></v-spacer>
+        <v-btn outlined icon color="primary" v-if="local" @click="isOpen=true">
+          <v-icon>mdi-folder-plus</v-icon>
+        </v-btn>
+        <v-btn outlined icon color="primary" v-if="isVisible && local" @click="deleteLastFolder(), btnUpVisibility()">
+          <v-icon>mdi-arrow-up</v-icon>
+        </v-btn>
       </v-toolbar>
+      <v-dialog
+        v-model="isOpen"
+        max-width="425"
+        persistent
+        :fullscreen="$vuetify.breakpoint.smAndDown"
+      >
+        <v-card>
+          <v-toolbar dark color="primary">
+            <v-btn icon dark @click="closeDialog(undefined)">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+            <v-toolbar-title>Input folder name</v-toolbar-title> 
+          </v-toolbar>
+          <v-form v-model="valid">
+            <BottomInput >
+              <v-text-field
 
+                  :label="$tc('Folder name')"
+                  filled
+                  clearable
+                  :rules="folderNameRules"
+                  @input="nameWasChanged = true"
+                  @click="nameKeyboard = true"
+              />
+            </BottomInput>
+            <v-btn :disabled="!valid">
+              Create
+            </v-btn>
+            <v-btn>
+              Cancel
+            </v-btn>
+          </v-form>
+        </v-card>
+      </v-dialog>
       <v-list
-        v-if="dataStorage.children.length > 0"
+        v-if="dataStorage && dataStorage.children.length > 0"
         two-line
         :style="styleObj"
         class="overflow-y-auto"
       >
         <v-list-item v-for="(file, index) in dataStorage.children" :key="index">
-          <v-list-item-content>
+            <v-list-item-icon v-if="file.type=='folder'">
+              <v-icon>mdi-folder</v-icon>
+            </v-list-item-icon>
+          <v-list-item-content :disabled="file.type=='machinecode'||file.type=='gcode'"
+            @click="addFolderToPath(file.name), btnUpVisibility()"
+          >
+
             <v-list-item-title class="subheading">
               {{ file.display }}
               <v-chip
@@ -30,8 +76,13 @@
             </v-list-item-title>
 
             <v-list-item-subtitle
+              v-if="file.type=='machinecode'||file.type=='gcode'"
               class="body-1"
             >{{$t("Uploaded ")}}{{ $moment(file.date).fromNow()}}</v-list-item-subtitle>
+            <v-list-item-subtitle
+              v-else
+              class="body-1"
+            >{{$t(file.size)}} {{$t("Kb")}}</v-list-item-subtitle>
           </v-list-item-content>
           <v-list-item-action>
             <v-menu>
@@ -103,6 +154,7 @@
 import { Vue, Component, Prop } from 'nuxt-property-decorator'
 import { State, Action, Getter, namespace } from 'vuex-class'
 import { FileOrFolder } from '~/types/fileOrFolder'
+import BottomInput from '~/components/common/BottomInput.vue'
 
 const storage = namespace('storageState')
 
@@ -113,7 +165,7 @@ export default class extends Vue {
   @Prop({ type: String }) display?: string
 
 
-  @storage.Getter localStorage!: FileOrFolder
+  @storage.Getter localStorage!: (path: string[]) => FileOrFolder | undefined
   @storage.Getter usbStorage!: (name: string) => FileOrFolder | undefined
 
   @storage.Action uploadFiles: any
@@ -128,10 +180,17 @@ export default class extends Vue {
   }
 
   private overlay: boolean = false
-
+  private isVisible: boolean = false
+  private isOpen: boolean = false
+  private nameWasChanged: boolean = false
+  private valid: boolean = false
   private files: File[] = []
+  private path: string[] = []
   //
 
+  private folderNameRules = [
+    v=>!!v || 'Folder name is required!'
+  ]
   showMenu: boolean = false
   menuX: number = 0
   menuY: number = 0
@@ -152,13 +211,45 @@ export default class extends Vue {
     this.overlay = false
   }
 
-  get dataStorage (): FileOrFolder | undefined {
+  private async closeDialog(add: boolean | undefined){
+    this.isOpen = false
+  }
+  get dataStorage () {
     if (this.local) {
-      return this.localStorage
+      return this.localStorage(this.path)
     } else if (this.name !== undefined) {
       return this.usbStorage(this.name)
     }
   }
+
+  private addFolderToPath(name: string | undefined){
+    if(this.path===undefined){
+      return this.path
+    }
+    else{
+      return this.path.push("/", name as string)
+    }
+
+  }
+
+  private deleteLastFolder(){
+    if(this.path==undefined){
+      return this.path
+    }
+    else{
+      return this.path.splice(this.path.length-1,1)
+    }
+  }
+
+  private btnUpVisibility(){
+    if(this.path.length==0){
+      this.isVisible=false 
+    }
+    else{
+      this.isVisible=true
+    }
+  }
+
 }
 </script>
 
