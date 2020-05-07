cd327e8c21efeaf889e8d71b38c70b6af9653fd9
Ilya Gushchin <frylock3434@gmail.com>
2020-05-07 10:10:00 +0400

    add page

---
+++ pages/manuals.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar
+      :collapse="!collapseOnScroll"
+      :collapse-on-scroll="collapseOnScroll"
+      absolute
+      color="primary"
+      dark
+      scroll-target="#scrolling-techniques-6"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Collapsing Bar</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-checkbox v-model="collapseOnScroll" color="white" hide-details></v-checkbox>
+    </v-app-bar>
+    <v-sheet id="scrolling-techniques-6" class="overflow-y-auto" max-height="600">
+      <v-container style="height: 1000px;"></v-container>
+    </v-sheet>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class Manuals extends Vue {
+  collapseOnScroll: boolean = true
+}
+
+</script>
+
+<style>
+</style>
