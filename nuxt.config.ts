99bcd91a684e27674a63e918a7fc3f98d70123c9
Ilya Gushchin <frylock3434@gmail.com>
2020-06-23 10:40:46 +0000

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,7 +72,7 @@ const config: Configuration = {
         light: {
           primary: '#0277bd',
           secondary: '#ffffff',
-          accent: '#263238',
+          accent: '#191b38',
           error: '#FF5252',
           info: '#9C27B0',
           success: '#4CAF50',
@@ -81,7 +81,7 @@ const config: Configuration = {
         dark: {
           primary: '#0277bd',
           secondary: '#ffffff',
-          accent: '#263238',
+          accent: '#191b38',
           error: '#FF5252',
           info: '#9C27B0',
           success: '#4CAF50',
